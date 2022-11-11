de0c00262b5239668fb4ad4b85d7b2d87e9df745
sapho-sys <saphonkunzi@gmail.com>
2022-11-11 09:11:46 +0200

    progress

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const carsTemplateText = document.querySelector('.carsTemplate');
 const carsTemplate = Handlebars.compile(carsTemplateText.innerHTML);
-
 const carsElem = document.querySelector('.cars');
+
  axios.get('http://api-tutor.herokuapp.com/v1/cars')
  .then(result =>{
     const cars = result.data;
@@ -9,3 +9,32 @@ const carsElem = document.querySelector('.cars');
         cars
     });
  });
+
+ const colorTemplateText = document.querySelector('.colorsTemplate');
+ const colorsTemplate = Handlebars.compile(colorTemplateText.innerHTML);
+ const colorElem = document.querySelector('.colors');
+ 
+
+ axios.get('https://api-tutor.herokuapp.com/v1/colors')
+  .then(result => {
+    const colors = result.data;
+    colorElem.innerHTML = colorsTemplate({
+        colors
+    })
+  });
+
+
+  const makesTemplateText = document.querySelector('.makeTemplate');
+  const makesTemplate = Handlebars.compile(makesTemplateText.innerHTML);
+  const makesElem = document.querySelector('.makes');
+
+  axios.get('https://api-tutor.herokuapp.com/v1/makes')
+   .then(result => {
+    const makes = result.data;
+    makesElem.innerHTML = makesTemplate({
+        makes
+    })
+   })
+
+
+ 
