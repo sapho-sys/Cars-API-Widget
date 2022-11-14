8f1b96f1b38be4864bdbca2e2920972cdcca128e
sapho-sys <saphonkunzi@gmail.com>
2022-11-14 12:21:25 +0200

    filter logic

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const carsTemplateText = document.querySelector('.carsTemplate');
-const carsTemplate = Handlebars.compile(carsTemplateText.innerHTML);
-const carsElem = document.querySelector('.cars');
-
-axios.get('http://api-tutor.herokuapp.com/v1/cars')
-    .then(result => {
-        const cars = result.data;
-        carsElem.innerHTML = carsTemplate({
-            cars
-        });
-    });
-
 const colorTemplateText = document.querySelector('.colorsTemplate');
 const colorsTemplate = Handlebars.compile(colorTemplateText.innerHTML);
 const colorElem = document.querySelector('.colors');
@@ -35,6 +23,47 @@ axios.get('https://api-tutor.herokuapp.com/v1/makes')
             makes
         });
     });
+
+    const carsTemplateText = document.querySelector('.carsTemplate');
+    const carsTemplate = Handlebars.compile(carsTemplateText.innerHTML);
+    const carsElem = document.querySelector('.cars');
+    axios.get('http://api-tutor.herokuapp.com/v1/cars')
+    .then(result => {
+        const cars = result.data;
+        carsElem.innerHTML = carsTemplate({
+            cars
+        });
+    });
+
+document.querySelector('.search').addEventListener('click', async () => {
+    if(colorElem.value){
+        await axios.get('http://api-tutor.herokuapp.com//v1/cars/color/${colorElem.value}')
+        .then(result => {
+            const data = result.data;
+            carsElem.innerHTML = carsTemplate({
+                data
+            })
+        })
+    }else if(makesElem.value){
+        await axios.get('http://api-tutor.herokuapp.com//v1/cars/color/${makesElem.value}')
+        .then(result =>{
+            const data = result.data;
+            carsElem.innerHTML = carsTemplate({
+                data
+            })
+        })
+    }else if(colorElem.value && makesElem.value){
+        await axios.get('http://api-tutor.herokuapp.com/v1/cars/make/${makesElem.value}/color/${colorElem.value}')
+        .then(result =>{
+            const data = result.data;
+            carsElem.innerHTML = carsTemplate({
+                data
+            })
+        })
+    }
+ 
+
+})
 
 
   
